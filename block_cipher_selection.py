4a5fe81c38a8c2c630f24d2b573eaa68ef700441
frosty1313 <10932308+frosty1313@users.noreply.github.com>
2022-12-08 19:30:27 -0500

    Add files via upload

---
+++ block_cipher_selection.py
@@ -0,0 +1,124 @@
+import sys
+import time
+try:
+    from prettytable import PrettyTable
+except:
+    print('Please run "pip3 install prettytable"')
+    sys.exit(0)
+
+class Block_Cipher_Selector:
+    def __init__(self):
+        self.modes = {'ECB': 0,
+                      'CBC': 5,
+                      'OFB': 5,
+                      'Counter': 10, #Start counter with highest bias
+                      'CWC': 5}
+    
+    def current_answer(self):
+        return sorted(self.modes.items(), key=lambda item:item[1], reverse=True)[0][0]
+    
+    def ask(self, text, valid_answers=['1','2']):
+        ans = None
+        while ans not in valid_answers:
+            ans = input(f'{text} > ')
+            print('')
+        return ans
+    
+    '''Check the importance of speed
+    ECB is made most relevant or least relevant depending on answer'''
+    def speed(self):
+        answer = self.ask('Which is most important? 1: speed, 2: security')
+        if answer == '1':
+            answer = self.ask('Are you SURE speed is most important? 1: yes, 2: no')
+            if answer == '1':
+                self.modes['ECB'] += 100
+                self.modes['Counter'] += 50
+        else:
+            self.modes['ECB'] -= 100
+            answer = self.ask('Ok, speed isn\'t most important, but do you still care? 1: yes, 2: no')
+            if answer == '1':
+                self.modes['ECB'] += 50
+            else:
+                for mode in self.modes:
+                    if mode != 'ECB':
+                        self.modes[mode] += 20
+    
+    def data_access(self):
+        answer = self.ask('How do you want to access your encrypted data? 1: in order that it was encrypted, 2: access any data you choose')
+        if answer == '1':
+            self.modes['CBC'] += 20
+        else:
+            self.modes['OFB'] += 20
+            self.modes['Counter'] += 20
+            self.modes['CWC'] += 20
+            self.modes['ECB'] += 20
+    
+    def stream(self):
+        answer = self.ask('Do you plan to be streaming your encrypted data, or will it be kept at rest (like on a hard drive? 1: Stream, 2: At rest')
+        if answer == '1':
+            self.modes['ECB'] += 20
+            self.modes['CBC'] += 20
+        else:
+            self.modes['OFB'] += 20
+            self.modes['Counter'] += 20
+            self.modes['CWC'] += 20
+    
+    def standardization(self):
+        answer = self.ask('Do you want your mode to be a NIST recommendation? 1: yes, 2: don\'t care') 
+        if answer == '1':
+            for mode in self.modes:
+                if mode != 'CWC':
+                    self.modes[mode] += 20
+
+    def print(self):
+        contents = {
+            'headers': ['Rank', 'Mode of Operation', 'Block/Stream', 'Runtime Efficiency', 'Security', 'Decrypted data Access', 'NIST Recommended?'],
+            'ECB': ['Electronic Code Book', 'Block', 'Fastest', 'Leaks block equality, may leak image', 'Random', 'Yes'],
+            'CBC': ['Cipher Block Chaining', 'Block', 'Fast enough for almost all applications', 'Supports IND-CPA if cipher is a Psuedo-Random Function', 'Sequential', 'Yes'],
+            'OFB': ['Output Feedback', 'Stream', 'Fast enough for almost all applications', 'Supports IND-CPA if cipher is a Psuedo-Random Function', 'Random', 'Yes'],
+            'Counter': ['Galois/Counter Mode', 'Stream', 'Faster for almost all applications', 'Supports IND-CPA if cipher is a Psuedo-Random Function', 'Random, O[data]', 'Yes'],
+            'CWC': ['Carter-Wegman Counter Mode', 'Stream', 'Fast enough for almost all applications', 'Supports IND-CPA if cipher is a Psuedo-Random Function', 'Random', 'No']
+        }
+            
+        table = PrettyTable()
+        table.field_names = contents['headers']
+        
+        #Sort the modes by score
+        sort = sorted(self.modes.items(), key=lambda item:item[1], reverse=True)
+        rank = 1
+        for item in sort:         
+            row = contents[item[0]]
+            row.insert(0, rank)
+            table.add_row(contents[item[0]])
+            rank+=1
+        
+        print(table)
+        
+    def run(self, test=False):
+        print('Welcome to our Block Cipher Selection Tool')
+        print('')
+        if not test:
+            time.sleep(2)
+        
+        print('Throughout, we will ask you a series of questions, you will answer by typing 1 or 2 and hitting enter')
+        print('')
+        if not test:
+            time.sleep(2)
+            
+        print('We recommend maximizing your terminal window for best viewing experience.')
+        if not test:
+            time.sleep(2)
+            
+        print('')
+        print('')
+        self.speed()
+        self.data_access()
+        self.stream()
+        self.standardization()
+        
+        self.print()
+        print('')
+        print('Thank you for using our advisor!')
+
+bcs = Block_Cipher_Selector()
+bcs.run(test=True)
